b183cace36f60dafffe888af1f56966ae3e25942
AbelWondimuL <abelwondimu208@gmail.com>
2023-09-04 16:04:47 -0700

    updated

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ load_dotenv()
 app = Flask(__name__)
 
 api_key = os.getenv("API_KEY")
+port = os.getenv("PORT")
 
 @app.route('/market')
 def main():
@@ -48,6 +49,6 @@ def contactUs():
 
 
 
-# if __name__ == "__main__":
-#     app.debug = True
-#     app.run(host='127.0.0.1', port=8000)
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='127.0.0.1', port=port)
